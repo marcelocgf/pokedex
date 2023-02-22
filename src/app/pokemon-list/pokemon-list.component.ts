0ec185bffe02ca2d955b5214a0b4d209920e9e86
Marcelo Cambraia Gennari Filho <marcelo.gennari@tradingworks.com.br>
2023-02-21 23:10:16 -0300

    feat: pokemon data

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Pokemon } from 'src/_model/pokemon';
+import { Type } from 'src/_model/type';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-list.component.sass']
 })
 export class PokemonListComponent {
-
+  public pokemons: Pokemon[] = [
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      number: 1,
+      name: 'Bulbasaur',
+      types: [
+        Type.Grass,
+        Type.Poison
+      ]
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
+      number: 2,
+      name: 'Ivysaur',
+      types: [
+        Type.Grass,
+        Type.Poison
+      ]
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
+      number: 3,
+      name: 'Bulbasaur',
+      types: [
+        Type.Grass,
+        Type.Poison
+      ]
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png',
+      number: 4,
+      name: 'Charmander',
+      types: [
+        Type.Fire
+      ]
+    }
+  ]
 }
+
+
+
